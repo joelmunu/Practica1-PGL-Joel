833e7004775a3f0a26283a4c1f35a50ab4a54f9a
joelmunu <joel.munu@gmail.com>
2022-10-03 19:18:28 +0100

    Feature: Finalizado el componente Body

--- components/Body/Body.js
+++ components/Body/Body.js
@@ -3,7 +3,11 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function Body() {
   return (
     <View style={styles.container}>
-        <Text>Body</Text>
+        <Text style={styles.body}>
+            Mi nombre es Joel Munuera Marrero, actualmente me encuentro estudiando el segundo curso del <Text style={styles.bold}>
+            CFGS DAM </Text><Text style={styles.grey}>(Desarrollo de aplicaciones Multiplataforma)</Text> en el colegio <Text style={styles.italic}>
+            Salesianos La Cuesta</Text>. Donde también cursé el <Text style={styles.bold}>CFGM SMR</Text>.
+        </Text>
     </View>
   );
 }
@@ -15,4 +19,22 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+
+  body: {
+    padding: 20,
+    textAlign: 'justify',
+    lineHeight: 30
+  },
+
+  italic: {
+    fontStyle: 'italic'
+  },
+
+  grey: {
+    color: 'grey'
+  },
+
+  bold: {
+    fontWeight: 'bold'
+  }
 });
