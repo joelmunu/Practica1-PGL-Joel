884e82805ae1bf2e0b872a6276787cb9fcaf710f
joelmunu <joel.munu@gmail.com>
2022-10-03 18:46:57 +0100

    Feature: Añadido y finalizado el componente Header y su llamada desde App.js

--- components/Header/Header.js
+++ components/Header/Header.js
@@ -1,17 +1,36 @@
-import { StyleSheet, Text, View } from 'react-native';
+import { Image, StyleSheet, Text, View } from 'react-native';
 
 export default function Header() {
     return (
       <View style={styles.container}>
+        <Image style={styles.img} source={require("./img/pic.jpg")}/>
+        <Text style={styles.name}>Joel Munuera Marrero</Text>
       </View>
     );
   }
   
   const styles = StyleSheet.create({
     container: {
-      flex: 1,
-      backgroundColor: '#fff',
+      flexDirection: 'row',
+      height: 150,
+      width: '100%',
+      backgroundColor: 'lemonchiffon',
       alignItems: 'center',
-      justifyContent: 'center',
+
     },
+
+    img: {
+       width: 80,
+       height: 80,
+       borderRadius: 50,
+       marginLeft: 15,
+       marginTop: 20,
+       marginRight: 40
+    },
+
+    name: {
+        color: 'deepskyblue',
+        fontSize: 20,
+        marginTop: 25
+    }
   });
